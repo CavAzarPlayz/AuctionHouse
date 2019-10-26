8f8ef4e1d1c21f4259c9688b1126a4e58a6a5a3f
CavAzarPlayz <57035898+CavAzarPlayz@users.noreply.github.com>
2019-10-26 19:19:33 +0700

    Update CooldownTask.php

--- src/sLeeD/AuctionHouseUpgraded/CooldownTask.php
+++ src/sLeeD/AuctionHouseUpgraded/CooldownTask.php
@@ -1,1 +1,21 @@
+<?php
+
+namespace sLeeD\AuctionHouseUpgraded;
+
+use pocketmine\scheduler\Task;
+
+class CooldownTask extends Task{
+
+    private $plugin;
+
+    public function __construct(Main $plugin)
+    {
+        $this->plugin = $plugin;
+    }
+
+
+    public function onRun($tick){
+        $this->plugin->timer();
+    }
+}
 
